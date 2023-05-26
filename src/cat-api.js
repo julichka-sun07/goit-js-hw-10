f6e34e3f9eb5870f4f826ebf4a64d5a5fb92a82b
julichka-sun07 <112427883+julichka-sun07@users.noreply.github.com>
2023-05-26 23:08:46 +0300

    hw-js10(2)

--- src/cat-api.js
+++ src/cat-api.js
@@ -13,3 +13,30 @@ export function fetchBreeds() {
         }));
       });
   }
+
+  export function fetchCatByBreed(breedId) {
+    const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+  
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch cat information.');
+        }
+        return response.json();
+      })
+      .then(data => {
+        return data;
+      })
+      .catch(error => {
+        throw error;
+      });
+  }
+
+// import API from "./index.js";
+
+// API.getNews("gpt-3").then((result) => console.log(result));
+
+
+
+    
+      
--- src/index.js
+++ src/index.js
@@ -18,3 +18,67 @@ fetchBreeds()
   .catch(error => {
     console.log(error);
   });
+
+  import { fetchCatByBreed } from './cat-api.js';
+
+const breedSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+
+breedSelect.addEventListener('change', () => {
+  const selectedBreedId = breedSelect.value;
+  
+  showLoader();
+
+  fetchCatByBreed(selectedBreedId)
+    .then(data => {
+      hideLoader();
+      showCatInfo(data);
+    })
+    .catch(error => {
+      hideLoader();
+      showError();
+      console.log(error);
+    });
+});
+function showLoader() {
+  const loader = document.querySelector('.loader');
+  loader.style.display = 'block';
+}
+
+function hideLoader() {
+  const loader = document.querySelector('.loader');
+  loader.style.display = 'none';
+}
+
+function showError() {
+  const error = document.querySelector('.error');
+  error.style.display = 'block';
+}
+
+function hideError() {
+  const error = document.querySelector('.error');
+  error.style.display = 'none';
+}
+
+function showCatInfo(cat) {
+  const { breeds, url } = cat;
+  const breed = breeds[0];
+
+  const catInfoHTML = `
+    <h2>Cat Information</h2>
+    <p><strong>Breed:</strong> ${breed.name}</p>
+    <p><strong>Description:</strong> ${breed.description}</p>
+    <p><strong>Temperament:</strong> ${breed.temperament}</p>
+    <img src="${url}" alt="Cat Image">
+  `;
+  catInfo.innerHTML = catInfoHTML;
+}
+
+
+// const URL = "https://api.thecatapi.com/v1/breeds";
+// const API_KEY = "live_CrqT7ZvnaVE9mwMgaNSEZPVbet7dJRSZ8ze5EJMIhCnHY9ZjrJZwwwCJIsZMMh22";
+
+// function getNews(quary){
+//   return fetch(`${URL}?apiKey=${API_KEY}&q=${quary}`).then((res) => res.json()); 
+// }
+// export default { getNews };
